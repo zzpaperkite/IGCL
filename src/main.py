5c2e93ade47e8e51e3e0744e9e0e59235fd9a4c1
zzpaperkite <108918582+zzpaperkite@users.noreply.github.com>
2024-06-03 19:33:25 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,23 @@
+import os
+import argparse
+from utils.quick_start import quick_start, quick_eval
+os.environ['NUMEXPR_MAX_THREADS'] = '48'
 
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', '-m', type=str, default='IGCL', help='name of models')
+    parser.add_argument('--dataset', '-d', type=str, default='sports', help='name of datasets')
+    parser.add_argument('--not_mg', action="store_true", help='whether to not use Mirror Gradient, default is False')
+    parser.add_argument('--resume', type=str, default=None)
+
+    config_dict = {
+        'gpu_id': 0,
+    }
+
+    args, _ = parser.parse_known_args()
+
+    if args.resume == None:
+        quick_start(model=args.model, dataset=args.dataset, config_dict=config_dict, save_model=True, mg=not args.not_mg)
+    else:
+        quick_eval(model=args.model, resume=args.resume, dataset=args.dataset, config_dict=config_dict)
